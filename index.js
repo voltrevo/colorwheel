3c6491e6bc875e336bb34750b33d815254acbaaf
Andrew Morris <voltrevo@gmail.com>
2018-03-21 07:11:33 +0000

    Many lines

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ const Html = (str) => {
   return wrapper.firstChild;
 };
 
+const Element = (tag, styles = {}) => {
+  const el = document.createElement(tag);
+
+  for (const [key, value] of Object.entries(styles)) {
+    el.style[key] = value;
+  }
+
+  return el;
+};
+
 const animLoop = (fn) => {
   let loop;
   window.requestAnimationFrame(loop = () => {
@@ -27,21 +37,45 @@ const animLoop = (fn) => {
   });
 };
 
+const range = n => (new Array(n)).fill(0).map((x, i) => i);
+
 window.addEventListener('load', () => {
   const start = Date.now();
 
-  document.body.appendChild(document.createTextNode('125'));
+  document.body.appendChild(document.createTextNode('123'));
+
+  const lines = range(100).map(i => ({
+    base: Element('div', {
+      width: '300px',
+      height: '1px',
+      position: 'absolute',
+      left: '10px',
+      top: `${150 - i}px`,
+    }),
+    progress: Element('div', {
+      width: '300px',
+      height: '1px',
+      position: 'absolute',
+      left: '10px',
+      top: `${150 - i}px`,
+    }),
+  }));
+
+  for (const { base, progress } of lines) {
+    document.body.appendChild(base);
+    document.body.appendChild(progress);
+  }
 
   animLoop(() => {
-    const block0 = document.querySelector('#block0');
-    const block1 = document.querySelector('#block1');
-    const display = document.querySelector('#display');
-
     const t = Date.now() - start;
 
-    block0.style.backgroundColor = Color(Math.floor(Math.log(t)));
-    block1.style.backgroundColor = Color(Math.ceil(Math.log(t)));
-    block1.style.width = `${300 * (Math.log(t) - Math.floor(Math.log(t)))}px`;
-    display.textContent = t;
+    for (let i = 0; i !== lines.length; i += 1) {
+      const { base, progress } = lines[i];
+      const ii = (i + 1) / 101; // 0 < ii < 1
+      const log = x => Math.log(x) + Math.log(ii / (1 - ii)) / 10;
+      base.style.backgroundColor = Color(Math.floor(log(t)));
+      progress.style.backgroundColor = Color(Math.ceil(log(t)));
+      progress.style.width = `${300 * (log(t) - Math.floor(log(t)))}px`;
+    }
   });
 });
